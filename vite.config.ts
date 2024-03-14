801237d4c7f38bdd2663dc2166b34c5e7d366f5d
Nydwhal <cyril.cauquil@gmail.com>
2024-03-14 09:34:48 +0100

    base dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "dev" ? "/dev/" : "/",
+  base: "/dev/",
   optimizeDeps: {
     include: ["@emotion/styled"],
-  },
-  server: {
-    proxy: {
-      // Ajoute "/dev" à la fin de chaque requête
-      "/api": {
-        target: "https://sample-restaurant.com/dev",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
 });
